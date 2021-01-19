df2d3bc0ea5082715b0173807756161f26fab4ce
Paradise <zdparadise@icloud.com>
2021-01-19 14:35:03 +0800

    睡眠管理：完善排序和更新接口

--- paradise-admin/src/main/java/com/paradise/core/service/impl/DaySleepRecordService.java
+++ paradise-admin/src/main/java/com/paradise/core/service/impl/DaySleepRecordService.java
@@ -32,13 +32,7 @@ public class DaySleepRecordService {
     public int insert(DaySleepRecordBody record) {
         DaySleepRecord daySleepRecord = new DaySleepRecord();
         BeanUtils.copyProperties(record, daySleepRecord);
-        daySleepRecord.setDate(new Date(record.getDate()));
-        daySleepRecord.setBedTime(new Date(record.getBedTime()));
-        daySleepRecord.setSleepTime(new Date(record.getSleepTime()));
-        daySleepRecord.setWakeTime(new Date(record.getWakeTime()));
-        daySleepRecord.setUpTime(new Date(record.getUpTime()));
-        // 计算睡眠时长
-        daySleepRecord.setDuration(cal(record));
+        dealRecord(record, daySleepRecord);
         daySleepRecord.setCreateAt(new Date());
         daySleepRecord.setUpdateAt(new Date());
         return this.daySleepRecordMapper.insertSelective(daySleepRecord);
@@ -68,11 +62,24 @@ public class DaySleepRecordService {
     public int updateByPrimaryKey(Long id, DaySleepRecordBody record) {
         DaySleepRecord daySleepRecord = new DaySleepRecord();
         BeanUtils.copyProperties(record, daySleepRecord);
-        return this.daySleepRecordMapper.updateByPrimaryKey(daySleepRecord);
+        daySleepRecord.setId(id);
+        dealRecord(record, daySleepRecord);
+        return this.daySleepRecordMapper.updateByPrimaryKeySelective(daySleepRecord);
+    }
+
+    private void dealRecord(DaySleepRecordBody record, DaySleepRecord daySleepRecord) {
+        daySleepRecord.setDate(new Date(record.getDate()));
+        daySleepRecord.setBedTime(new Date(record.getBedTime()));
+        daySleepRecord.setSleepTime(new Date(record.getSleepTime()));
+        daySleepRecord.setWakeTime(new Date(record.getWakeTime()));
+        daySleepRecord.setUpTime(new Date(record.getUpTime()));
+        // 计算睡眠时长
+        daySleepRecord.setDuration(cal(record));
     }
 
     public List<DaySleepRecord> selectByPage(DaySleepRecordQuery query) {
         PageHelper.startPage(query.getPageNum(), query.getPageSize());
-        return this.daySleepRecordMapper.selectByExample(new DaySleepRecordExample());
+        return this.daySleepRecordMapper.selectByExample(new DaySleepRecordExample()
+                .createCriteria().example().orderBy(DaySleepRecord.Column.date.desc()));
     }
 }
